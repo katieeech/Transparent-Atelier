4e03b5bc55ab1e45c833009a1fe10388698df09a
Katie Hyewon Choi <hyewonchoi31@gmail.com>
2021-10-27 17:02:33 +0900

    Revised button styling

--- src/Container/MainHomeContainer.js
+++ src/Container/MainHomeContainer.js
@@ -5,16 +5,26 @@ import EyeGlassesImage from '../images/eyeglasses4.jpg'
 export default class MainHomeContainer extends Component {
     render() {
         return (
+     
             <div className="home-image-container">
                 {/* <Helmet bodyAttributes={{ style: 'background-color : #000' }} /> */}
-
+                <div className="home-inner"> 
     <div className="home-text">
-        <div><h2>Shop Sunglasses</h2>
-    <p>Each pair is equipped with scratch-resistant lenses that block 100% of UVA and UVB rays. Starting at $95, with free shipping and free returns.
-</p></div>
-    
- 
+        <div>
+            <button onClick={() => {
+                        this.props.history.push("/collection")
+                        this.props.showOnlyTint()
+                    }}
+                        className="shop-glasses-btn" >
+                              Shop Sunglasses
+             </button>
+             <p>Each pair is equipped with scratch-resistant lenses
+         that block 100% of UVA and UVB rays.
+          Starting at $95, with free shipping and free returns.
+             </p>
+             </div>
     </div>
+
     <div className="home-img">
     <img src={HomeImage}></img>
     </div>
@@ -24,30 +34,22 @@ export default class MainHomeContainer extends Component {
     </div>
 
     <div className="home-text">
-    <div><h2>Shop eyeglasses</h2>
-    <p>Each pair is equipped with scratch-resistant lenses that block 100% of UVA and UVB rays. Starting at $95, with free shipping and free returns.
-</p></div>
-       </div>
-
-{/* <button onClick={() => {
+      <div>
+      <button onClick={() => {
                         this.props.history.push("/collection")
                         this.props.showOnlyClear()
                     }}
-                        className="clear-lenses-btn" >
-                        Eyeglasses
+                        className="shop-glasses-btn" >
+                        Shop Eyeglasses
                         </button>
-                        <button onClick={() => {
-                        this.props.history.push("/collection")
-                        this.props.showOnlyTint()
-                    }}
-                        className="tinted-lenses-btn">
-                        Sunglasses
-                </button> */}
+        <p>Each pair is equipped with scratch-resistant lenses that block 100% of UVA and UVB rays. Starting at $95, with free shipping and free returns.
+        </p>
+      </div>
+    </div>
 
+  </div>
 
-
-
-            </div >
+</div >
         )
     }
 }
